3f841430704efe06f597ae11d297541168624752
Herwig Hochleitner <herwig@bendlas.net>
2016-02-14 05:29:08 +0100

    release 0.0.1

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject lein-nix "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lein-nix "0.0.1"
+  :description "Generate nix expressions from leiningen
+  - classpath derivations
+  - launchers for leiningen tasks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
