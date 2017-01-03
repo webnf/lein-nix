b95c7643964bf3ae5fd28bb875ab38eff6ed4259
Herwig Hochleitner <herwig@bendlas.net>
2017-01-03 11:04:25 +0100

    WIP

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   - launchers for leiningen tasks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.apache.maven/maven-aether-provider "3.3.9"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.apache.maven/maven-aether-provider "3.3.9"]
                  [org.eclipse.aether/aether-transport-file "1.1.0"]
                  [org.eclipse.aether/aether-transport-wagon "1.1.0"]
                  [org.eclipse.aether/aether-connector-basic "1.1.0"]
                  [org.apache.maven.wagon/wagon-provider-api "2.10"]
                  [org.apache.maven.wagon/wagon-http "2.10"]
                  [org.apache.maven.wagon/wagon-ssh "2.10"]]
-  :eval-in-leiningen true)
+  ;:eval-in-leiningen true
+  )
---
+++ repo-generated.nix
@@ -0,0 +1,277 @@
+{
+  checksums = {
+      commons-lang = {
+          commons-lang = {
+              "2.6" = ["mvn" "0ce1edb914c94ebc388f086c6827e8bdeec71ac2"];
+            };
+        };
+      "org.apache.httpcomponents" = {
+          httpclient = {
+              "4.3.5" = ["mvn" "9783d89b8eea20a517a4afc5f979bd2882b54c44"];
+            };
+          httpcore = {
+              "4.3.2" = ["mvn" "31fbbff1ddbf98f3aa7377c94d33b0447c646b6e"];
+            };
+        };
+      "org.eclipse.aether" = {
+          aether-api = {
+              "1.0.2.v20150114" = ["mvn" "839f93a5213fb3e233b09bfd6d6b95669f7043c0"];
+              "1.1.0" = ["mvn" "05dd291e788f50dfb48822dab29defc16ad70860"];
+            };
+          aether-spi = {
+              "1.0.2.v20150114" = ["mvn" "8428dfa330107984f3e3ac05cc3ebd50b2676866"];
+              "1.1.0" = ["mvn" "a532baa7b965d2893ceace03e804b1882f448052"];
+            };
+          aether-util = {
+              "1.0.2.v20150114" = ["mvn" "d2d3c74a5210544b5cdce89a2c1d1c62835692d1"];
+              "1.1.0" = ["mvn" "d0f84ba994f0133580e8b957a44593eaa03d42cd"];
+            };
+          aether-impl = {
+              "1.0.2.v20150114" = ["mvn" "f147539e6e60dfbda9ef7f6d750066170f61b7a1"];
+            };
+aether-transport-file = {
+    "1.1.0" = ["mvn" "5c257319ccba22a02fa583c3946bfcbb47638bc8"];
+  };
+aether-transport-wagon = {
+    "1.1.0" = ["mvn" "5aeaa19811ae01f4ec6511e83304120679e47b45"];
+  };
+aether-connector-basic = {
+    "1.1.0" = ["mvn" "f5c784bdd704ff64166c086eb6b31e2784c87b66"];
+  };
+        };
+      junit = {
+          junit = {
+              "3.8.1" = ["mvn" "99129f16442844f6a4a11ae22fbbee40b14d774f"];
+            };
+        };
+commons-io = {
+    commons-io = {
+        "2.2" = ["mvn" "83b5b8a7ba1c08f9e8c8ff2373724e33d3c1e22a"];
+      };
+  };
+commons-logging = {
+    commons-logging = {
+        "1.1.3" = ["mvn" "f6f66e966c70a83ffbdb6f17a0919eaf7c8aca7f"];
+      };
+  };
+"org.apache.commons" = {
+    "commons-lang3" = {
+        "3.4" = ["mvn" "5fe28b9518e58819180a43a850fbc0dd24b7c050"];
+      };
+  };
+"org.apache.maven" = {
+    maven-aether-provider = {
+        "3.3.9" = ["mvn" "29e8e7122f7a166ea53785cd75af0ef9d4d848d4"];
+      };
+    maven-model = {
+        "3.3.9" = ["mvn" "6efde8cbcb4de4c47f7e9c2a3ab2806022b5c70f"];
+      };
+    maven-model-builder = {
+        "3.3.9" = ["mvn" "e2055f9adb9f3c9a93e6b36fffe79781a785de2d"];
+      };
+    maven-artifact = {
+        "3.3.9" = ["mvn" "0f43afa184555fbc6e36b3334b17246c39b30f6e"];
+      };
+maven-builder-support = {
+    "3.3.9" = ["mvn" "a96f29da7623c0e1db9824f628548fe8181f6dd0"];
+  };
+maven-repository-metadata = {
+    "3.3.9" = ["mvn" "6850232b35e504057d67bde11efddebf6271e1ce"];
+  };
+  };
+"com.jcraft" = {
+    jsch = {
+        "0.1.50" = ["mvn" "fae4a0b1f2a96cb8f58f38da2650814c991cea01"];
+        "0.1.49" = ["mvn" "5c46cae7d01e845379beac44512e25eb11017159"];
+      };
+    "jsch.agentproxy.connector-factory" = {
+        "0.0.6" = ["mvn" "250ab907c35a1777e70d54038086e84855d74aea"];
+      };
+    "jsch.agentproxy.core" = {
+        "0.0.6" = ["mvn" "fb27d5a19479c7617c74aaca4f1d38063ac7a25e"];
+      };
+    "jsch.agentproxy.usocket-jna" = {
+        "0.0.6" = ["mvn" "e5e027a2d77296a89088820d5b3ffd2705f513f1"];
+      };
+"jsch.agentproxy.usocket-nc" = {
+    "0.0.6" = ["mvn" "01ae336deaeb39fd712f9134b24343094901c03a"];
+  };
+"jsch.agentproxy.sshagent" = {
+    "0.0.6" = ["mvn" "c552a54fece150f69350fa11b9c28d0e455bcf4b"];
+  };
+"jsch.agentproxy.pageant" = {
+    "0.0.6" = ["mvn" "a4b88191bed87d3210d9666071b15b98e0447484"];
+  };
+"jsch.agentproxy.jsch" = {
+    "0.0.6" = ["mvn" "aee40b481ad104a578584335b4859e49d4be4f7a"];
+  };
+  };
+"org.clojure" = {
+    clojure = {
+        "1.9.0-alpha14" = ["mvn" "fa096515988cec810a2c75cdbd74d7d3826d414f"];
+      };
+  };
+"com.google.guava" = {
+    guava = {
+        "18.0" = ["mvn" "cce0823396aa693798f8882e64213b1772032b09"];
+      };
+  };
+commons-codec = {
+    commons-codec = {
+        "1.6" = ["mvn" "b7f0fc8f61ecadeb3695f0b9464755eee44374d4"];
+      };
+  };
+"org.codehaus.plexus" = {
+    plexus-interpolation = {
+        "1.21" = ["mvn" "f92de59d295f16868001644acc21720f3ec9eb15"];
+      };
+    plexus-component-annotations = {
+        "1.6" = ["mvn" "1a34a4e12b5fded8c548a568f463dfee21500927"];
+      };
+    plexus-utils = {
+        "3.0.22" = ["mvn" "764f26e0ab13a87c48fe55f525dfb6a133b7a92f"];
+        "1.4.2" = ["mvn" "b16d1eb4814a23d532100c868f3ed814ec1c74f6"];
+        "3.0.15" = ["mvn" "519b3f1fc84d3387c61a8c8e6235daf89aea4168"];
+        "1.4" = ["mvn" "9383ed6b6798c8909b2c842644757bbcb79cc8c3"];
+      };
+    plexus-interactivity-api = {
+        "1.0-alpha-6" = ["mvn" "c06f0eb818633033f09a87d14c4cfb6f39af9a37"];
+      };
+plexus-component-api = {
+    "1.0-alpha-16" = ["mvn" "5a2100a1c6a37804b1abfc70000b0ea33b83b7f9"];
+  };
+plexus-classworlds = {
+    "1.2-alpha-7" = ["mvn" "ed03d1eeb9b2576747df0d2883d9006fa5e1febe"];
+  };
+  };
+"org.jsoup" = {
+    jsoup = {
+        "1.7.2" = ["mvn" "d7e275ba05aa380ca254f72d0c0ffebaedc3adcf"];
+      };
+  };
+"org.apache.maven.wagon" = {
+    wagon-provider-api = {
+        "1.0" = ["mvn" "12bcd211158c0c09b027d06b70371861398fedea"];
+        "2.10" = ["mvn" "0cd9cdde3f56bb5250d87c54592f04cbc24f03bf"];
+      };
+    wagon-http = {
+        "2.10" = ["mvn" "b611dd343b14014af6201dd87d121b8d9a7eb5e1"];
+      };
+    wagon-http-shared = {
+        "2.10" = ["mvn" "3b96251214df697e902c849eb0b4a0efa2cd1a53"];
+      };
+    wagon-ssh = {
+        "2.10" = ["mvn" "e7522d82f1a026fd52e1b2b80d4decece8462180"];
+      };
+wagon-ssh-common = {
+    "2.10" = ["mvn" "edb124539696c2aa0d1c89ca342594a13fa82d2b"];
+  };
+  };
+"net.java.dev.jna" = {
+    jna = {
+        "3.4.0" = ["mvn" "803ff252fedbd395baffd43b37341dc4a150a554"];
+      };
+    platform = {
+        "3.4.0" = ["mvn" "e3f70017be8100d3d6923f50b3d2ee17714e9c13"];
+      };
+  };
+    };
+  dependencies = {
+      "org.apache.maven" = {
+          maven-aether-provider = {
+              "3.3.9" = [["org.apache.maven" "maven-model" "3.3.9"] ["org.apache.maven" "maven-model-builder" "3.3.9"] ["org.apache.maven" "maven-repository-metadata" "3.3.9"] ["org.eclipse.aether" "aether-api" "1.0.2.v20150114"] ["org.eclipse.aether" "aether-spi" "1.0.2.v20150114"] ["org.eclipse.aether" "aether-util" "1.0.2.v20150114"] ["org.eclipse.aether" "aether-impl" "1.0.2.v20150114"] ["org.codehaus.plexus" "plexus-component-annotations" "1.6"] ["org.codehaus.plexus" "plexus-utils" "3.0.22"] ["org.apache.commons" "commons-lang3" "3.4"]];
+            };
+          maven-model = {
+              "3.3.9" = [["org.codehaus.plexus" "plexus-utils" "3.0.22"] ["org.apache.commons" "commons-lang3" "3.4"]];
+            };
+          maven-model-builder = {
+              "3.3.9" = [["org.codehaus.plexus" "plexus-utils" "3.0.22"] ["org.codehaus.plexus" "plexus-interpolation" "1.21"] ["org.codehaus.plexus" "plexus-component-annotations" "1.6"] ["org.apache.maven" "maven-model" "3.3.9"] ["org.apache.maven" "maven-artifact" "3.3.9"] ["org.apache.maven" "maven-builder-support" "3.3.9"] ["com.google.guava" "guava" "18.0"] ["org.apache.commons" "commons-lang3" "3.4"]];
+            };
+          maven-artifact = {
+              "3.3.9" = [["org.codehaus.plexus" "plexus-utils" "3.0.22"] ["org.apache.commons" "commons-lang3" "3.4"]];
+            };
+maven-builder-support = {
+    "3.3.9" = [["org.codehaus.plexus" "plexus-utils" "3.0.22"] ["org.apache.commons" "commons-lang3" "3.4"]];
+  };
+maven-repository-metadata = {
+    "3.3.9" = [["org.codehaus.plexus" "plexus-utils" "3.0.22"]];
+  };
+        };
+      "org.eclipse.aether" = {
+          aether-spi = {
+              "1.0.2.v20150114" = [["org.eclipse.aether" "aether-api" "1.0.2.v20150114"]];
+              "1.1.0" = [["org.eclipse.aether" "aether-api" "1.1.0"]];
+            };
+          aether-util = {
+              "1.0.2.v20150114" = [["org.eclipse.aether" "aether-api" "1.0.2.v20150114"]];
+              "1.1.0" = [["org.eclipse.aether" "aether-api" "1.1.0"]];
+            };
+          aether-impl = {
+              "1.0.2.v20150114" = [["org.eclipse.aether" "aether-api" "1.0.2.v20150114"] ["org.eclipse.aether" "aether-spi" "1.0.2.v20150114"] ["org.eclipse.aether" "aether-util" "1.0.2.v20150114"]];
+            };
+          aether-transport-file = {
+              "1.1.0" = [["org.eclipse.aether" "aether-api" "1.1.0"] ["org.eclipse.aether" "aether-spi" "1.1.0"] ["org.eclipse.aether" "aether-util" "1.1.0"]];
+            };
+aether-transport-wagon = {
+    "1.1.0" = [["org.eclipse.aether" "aether-api" "1.1.0"] ["org.eclipse.aether" "aether-spi" "1.1.0"] ["org.eclipse.aether" "aether-util" "1.1.0"] ["org.apache.maven.wagon" "wagon-provider-api" "1.0"]];
+  };
+aether-connector-basic = {
+    "1.1.0" = [["org.eclipse.aether" "aether-api" "1.1.0"] ["org.eclipse.aether" "aether-spi" "1.1.0"] ["org.eclipse.aether" "aether-util" "1.1.0"]];
+  };
+        };
+      "org.apache.maven.wagon" = {
+          wagon-provider-api = {
+              "1.0" = [["org.codehaus.plexus" "plexus-utils" "1.4.2"]];
+              "2.10" = [["org.codehaus.plexus" "plexus-utils" "3.0.15"]];
+            };
+          wagon-http = {
+              "2.10" = [["org.apache.maven.wagon" "wagon-http-shared" "2.10"] ["org.apache.httpcomponents" "httpclient" "4.3.5"] ["commons-logging" "commons-logging" "1.1.3"] ["org.apache.httpcomponents" "httpcore" "4.3.2"] ["org.apache.maven.wagon" "wagon-provider-api" "2.10"]];
+            };
+          wagon-http-shared = {
+              "2.10" = [["org.jsoup" "jsoup" "1.7.2"] ["commons-lang" "commons-lang" "2.6"] ["commons-io" "commons-io" "2.2"] ["org.apache.maven.wagon" "wagon-provider-api" "2.10"]];
+            };
+          wagon-ssh = {
+              "2.10" = [["com.jcraft" "jsch" "0.1.50"] ["com.jcraft" "jsch.agentproxy.connector-factory" "0.0.6"] ["com.jcraft" "jsch.agentproxy.jsch" "0.0.6"] ["org.codehaus.plexus" "plexus-utils" "3.0.15"] ["org.apache.maven.wagon" "wagon-ssh-common" "2.10"] ["org.apache.maven.wagon" "wagon-provider-api" "2.10"]];
+            };
+wagon-ssh-common = {
+    "2.10" = [["org.codehaus.plexus" "plexus-utils" "3.0.15"] ["org.codehaus.plexus" "plexus-interactivity-api" "1.0-alpha-6"] ["org.apache.maven.wagon" "wagon-provider-api" "2.10"]];
+  };
+        };
+      "org.apache.httpcomponents" = {
+          httpclient = {
+              "4.3.5" = [["org.apache.httpcomponents" "httpcore" "4.3.2"] ["commons-logging" "commons-logging" "1.1.3"] ["commons-codec" "commons-codec" "1.6"]];
+            };
+        };
+"com.jcraft" = {
+    "jsch.agentproxy.connector-factory" = {
+        "0.0.6" = [["com.jcraft" "jsch.agentproxy.core" "0.0.6"] ["com.jcraft" "jsch.agentproxy.usocket-jna" "0.0.6"] ["com.jcraft" "jsch.agentproxy.usocket-nc" "0.0.6"] ["com.jcraft" "jsch.agentproxy.sshagent" "0.0.6"] ["com.jcraft" "jsch.agentproxy.pageant" "0.0.6"]];
+      };
+    "jsch.agentproxy.usocket-jna" = {
+        "0.0.6" = [["com.jcraft" "jsch.agentproxy.core" "0.0.6"] ["net.java.dev.jna" "jna" "3.4.0"] ["net.java.dev.jna" "platform" "3.4.0"]];
+      };
+    "jsch.agentproxy.usocket-nc" = {
+        "0.0.6" = [["com.jcraft" "jsch.agentproxy.core" "0.0.6"]];
+      };
+    "jsch.agentproxy.sshagent" = {
+        "0.0.6" = [["com.jcraft" "jsch.agentproxy.core" "0.0.6"]];
+      };
+"jsch.agentproxy.pageant" = {
+    "0.0.6" = [["com.jcraft" "jsch.agentproxy.core" "0.0.6"] ["net.java.dev.jna" "jna" "3.4.0"] ["net.java.dev.jna" "platform" "3.4.0"]];
+  };
+"jsch.agentproxy.jsch" = {
+    "0.0.6" = [["com.jcraft" "jsch" "0.1.49"] ["com.jcraft" "jsch.agentproxy.core" "0.0.6"]];
+  };
+  };
+"org.codehaus.plexus" = {
+    plexus-interactivity-api = {
+        "1.0-alpha-6" = [["org.codehaus.plexus" "plexus-utils" "1.4"] ["org.codehaus.plexus" "plexus-component-api" "1.0-alpha-16"]];
+      };
+    plexus-component-api = {
+        "1.0-alpha-16" = [["org.codehaus.plexus" "plexus-classworlds" "1.2-alpha-7"] ["junit" "junit" "3.8.1"]];
+      };
+    plexus-classworlds = {
+        "1.2-alpha-7" = [["junit" "junit" "3.8.1"]];
+      };
+  };
+    };
+}
